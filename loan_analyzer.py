647ac38a3d28576b67f48afe9a33eeda8820dbf3
Philippe Timothe <PhilippeTimothe@gmail.com>
2023-03-22 19:48:32 -0400

    Completion of part 1 through 4

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -18,17 +18,24 @@ loan_costs = [500, 600, 200, 1000, 450]
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
 # YOUR CODE HERE!
+total_loans = len(loan_costs)
+print(total_loans)
 
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
 # YOUR CODE HERE!
+total_loans_value = sum(loan_costs)
+print(total_loans_value)
 
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
 # YOUR CODE HERE!
+average_loan_amount = total_loans_value/total_loans
+print(average_loan_amount)
 
+#.............................................................................
 """Part 2: Analyze Loan Data.
 
 Analyze the loan to determine the investment evaluation.
@@ -63,14 +70,18 @@ loan = {
 # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
 # Print each variable.
 # YOUR CODE HERE!
-
+future_value = loan.get("future_value")
+remaining_months = loan.get("remaining_months")
+print(f"Future Value: {future_value}, Remaining Months: {remaining_months}")
 
 # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
 # Use a minimum required return of 20% as the discount rate.
 #   You'll want to use the **monthly** version of the present value formula.
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
-
 # YOUR CODE HERE!
+fair_value = future_value/(1 + (0.2/12))**remaining_months
+fair_value = round(fair_value, 2)
+print(f"Fair Value = {fair_value}")
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
@@ -78,7 +89,13 @@ loan = {
 #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
 # YOUR CODE HERE!
 
+if fair_value >= loan.get("loan_price"):
+    print("The loan is worth at least the cost to buy it.")
+else:
+    print("The loan is too expensive and not worth the price.")
+    
 
+#.............................................................................
 """Part 3: Perform Financial Calculations.
 
 Perform financial calculations using functions.
@@ -102,11 +119,16 @@ new_loan = {
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
 # YOUR CODE HERE!
+def calculate_present_value(future_value, remaining_months, annual_discount_rate):
+    #Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
+    present_value = future_value / (1 + (annual_discount_rate / 12)) ** remaining_months
+    return present_value
 
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
+present_value = round(calculate_present_value(new_loan.get("future_value"), new_loan.get("remaining_months"), 0.2), 2)
 print(f"The present value of the loan is: {present_value}")
 
 
@@ -150,14 +172,20 @@ loans = [
 
 # @TODO: Create an empty list called `inexpensive_loans`
 # YOUR CODE HERE!
+inexpensive_loans = []
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
 # YOUR CODE HERE!
+for loan in loans:
+    if loan.get("loan_price") <= 500:
+        inexpensive_loans.append(loan)
 
 # @TODO: Print the `inexpensive_loans` list
 # YOUR CODE HERE!
+print(inexpensive_loans)
 
 
+#.............................................................................
 """Part 5: Save the results.
 
 Output this list of inexpensive loans to a csv file
